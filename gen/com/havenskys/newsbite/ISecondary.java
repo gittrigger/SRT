dbb0f95d7b622857b079754e421aa3613484147f
Haven Skys <havenskys@gmail.com>
2013-03-30 16:33:16 -0700

    in

---
+++ gen/com/havenskys/newsbite/ISecondary.java
@@ -0,0 +1,139 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ * Original file: /Users/haven/Documents/workspace/June/src/com/havenskys/newsbite/ISecondary.aidl
+ */
+package com.havenskys.newsbite;
+import java.lang.String;
+import android.os.RemoteException;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Binder;
+import android.os.Parcel;
+public interface ISecondary extends android.os.IInterface
+{
+/** Local-side IPC implementation stub class. */
+public static abstract class Stub extends android.os.Binder implements com.havenskys.newsbite.ISecondary
+{
+private static final java.lang.String DESCRIPTOR = "com.havenskys.thescoopseattle.ISecondary";
+/** Construct the stub at attach it to the interface. */
+public Stub()
+{
+this.attachInterface(this, DESCRIPTOR);
+}
+/**
+ * Cast an IBinder object into an ISecondary interface,
+ * generating a proxy if needed.
+ */
+public static com.havenskys.newsbite.ISecondary asInterface(android.os.IBinder obj)
+{
+if ((obj==null)) {
+return null;
+}
+android.os.IInterface iin = (android.os.IInterface)obj.queryLocalInterface(DESCRIPTOR);
+if (((iin!=null)&&(iin instanceof com.havenskys.newsbite.ISecondary))) {
+return ((com.havenskys.newsbite.ISecondary)iin);
+}
+return new com.havenskys.newsbite.ISecondary.Stub.Proxy(obj);
+}
+public android.os.IBinder asBinder()
+{
+return this;
+}
+public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+{
+switch (code)
+{
+case INTERFACE_TRANSACTION:
+{
+reply.writeString(DESCRIPTOR);
+return true;
+}
+case TRANSACTION_getPid:
+{
+data.enforceInterface(DESCRIPTOR);
+int _result = this.getPid();
+reply.writeNoException();
+reply.writeInt(_result);
+return true;
+}
+case TRANSACTION_basicTypes:
+{
+data.enforceInterface(DESCRIPTOR);
+int _arg0;
+_arg0 = data.readInt();
+long _arg1;
+_arg1 = data.readLong();
+boolean _arg2;
+_arg2 = (0!=data.readInt());
+float _arg3;
+_arg3 = data.readFloat();
+double _arg4;
+_arg4 = data.readDouble();
+java.lang.String _arg5;
+_arg5 = data.readString();
+this.basicTypes(_arg0, _arg1, _arg2, _arg3, _arg4, _arg5);
+reply.writeNoException();
+return true;
+}
+}
+return super.onTransact(code, data, reply, flags);
+}
+private static class Proxy implements com.havenskys.newsbite.ISecondary
+{
+private android.os.IBinder mRemote;
+Proxy(android.os.IBinder remote)
+{
+mRemote = remote;
+}
+public android.os.IBinder asBinder()
+{
+return mRemote;
+}
+public java.lang.String getInterfaceDescriptor()
+{
+return DESCRIPTOR;
+}
+public int getPid() throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+int _result;
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+mRemote.transact(Stub.TRANSACTION_getPid, _data, _reply, 0);
+_reply.readException();
+_result = _reply.readInt();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+return _result;
+}
+public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double dDouble, java.lang.String aString) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeInt(anInt);
+_data.writeLong(aLong);
+_data.writeInt(((aBoolean)?(1):(0)));
+_data.writeFloat(aFloat);
+_data.writeDouble(dDouble);
+_data.writeString(aString);
+mRemote.transact(Stub.TRANSACTION_basicTypes, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+}
+static final int TRANSACTION_getPid = (IBinder.FIRST_CALL_TRANSACTION + 0);
+static final int TRANSACTION_basicTypes = (IBinder.FIRST_CALL_TRANSACTION + 1);
+}
+public int getPid() throws android.os.RemoteException;
+public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double dDouble, java.lang.String aString) throws android.os.RemoteException;
+}
